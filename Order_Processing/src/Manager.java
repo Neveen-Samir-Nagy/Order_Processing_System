703afd161524804ee50784155195befddb693166
Neveen Samir <48647031+Neveen-Samir-Nagy@users.noreply.github.com>
2020-05-08 19:07:01 +0200

    Manager

--- Order_Processing/src/Manager.java
+++ Order_Processing/src/Manager.java
@@ -1,5 +1,5 @@
 
-public class Manager implements Managers{
+public class Manager implements Customers, Managers{
 
 	@Override
 	public void add_NewBook() {
@@ -27,6 +27,54 @@ public class Manager implements Managers{
 
 	@Override
 	public void view_GroupingOnSales() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean sign_UP() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean sign_IN() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void edit_Information() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean search_ForBooks() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void add_BooksToShoppingCart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void manage_ShoppingCart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkOut_ShoppingCart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void log_Out() {
 		// TODO Auto-generated method stub
 		
 	}
