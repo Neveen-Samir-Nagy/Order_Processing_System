5346a16b7ea41648d0b0036fcb55a37089b5e869
Neveen Samir <48647031+Neveen-Samir-Nagy@users.noreply.github.com>
2020-05-08 19:01:58 +0200

    Classes

---
+++ Order_Processing/src/Customer.java
@@ -0,0 +1,52 @@
+
+public class Customer implements Customers{
+
+	@Override
+	public boolean sign_UP() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean sign_IN() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void edit_Information() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean search_ForBooks() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void add_BooksToShoppingCart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void manage_ShoppingCart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkOut_ShoppingCart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void log_Out() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Order_Processing/src/Manager.java
@@ -0,0 +1,34 @@
+
+public class Manager implements Managers{
+
+	@Override
+	public void add_NewBook() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modify_ExistingBook() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void place_OrdersForBooks() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void promote_RegisteredCustomersToHaveManagersCredentials() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void view_GroupingOnSales() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
