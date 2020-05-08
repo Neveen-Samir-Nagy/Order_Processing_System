14d338c80c5131b41463ec5d500835a0daf7f733
Neveen Samir <48647031+Neveen-Samir-Nagy@users.noreply.github.com>
2020-05-08 18:57:32 +0200

    Inters

--- Order_Processing/src/Customers.java
+++ Order_Processing/src/Customers.java
@@ -2,11 +2,18 @@
 public interface Customers {
 
 	boolean sign_UP();
+
 	boolean sign_IN();
+
 	void edit_Information();
+
 	boolean search_ForBooks();
+
 	void add_BooksToShoppingCart();
+
 	void manage_ShoppingCart();
+
 	void checkOut_ShoppingCart();
+
 	void log_Out();
 }
--- Order_Processing/src/Managers.java
+++ Order_Processing/src/Managers.java
@@ -2,8 +2,12 @@
 public interface Managers {
 
 	void add_NewBook();
+
 	void modify_ExistingBook();
+
 	void place_OrdersForBooks();
+
 	void promote_RegisteredCustomersToHaveManagersCredentials();
+
 	void view_GroupingOnSales();
 }
