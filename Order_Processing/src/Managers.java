a42880dc7ad8924215b08a5c1431cff4497bd913
Neveen Samir <48647031+Neveen-Samir-Nagy@users.noreply.github.com>
2020-05-08 18:56:38 +0200

    Interfaces

--- Order_Processing/src/Managers.java
+++ Order_Processing/src/Managers.java
@@ -1,4 +1,9 @@
 
 public interface Managers {
 
+	void add_NewBook();
+	void modify_ExistingBook();
+	void place_OrdersForBooks();
+	void promote_RegisteredCustomersToHaveManagersCredentials();
+	void view_GroupingOnSales();
 }
