51048189f9ab0c4833773cf7aa0186088cd2bdfc
Neveen Samir <48647031+Neveen-Samir-Nagy@users.noreply.github.com>
2020-05-11 05:06:33 +0200

    Manager&Customer

--- Order_Processing/src/ControllerCustomers.java
+++ Order_Processing/src/ControllerCustomers.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -24,6 +25,8 @@ import javafx.scene.control.Control;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
@@ -38,10 +41,72 @@ public class ControllerCustomers {
 	
 	Button log_out = new Button();
 	@FXML
-	TreeView<String> treeViewer;
+	Menu edit = new Menu();
+	@FXML
+	Menu profile = new Menu();
+	@FXML
+	Menu insert = new Menu();
+	@FXML
+	Menu delete = new Menu();
+	@FXML
+	Menu view = new Menu();
+	@FXML
+	Menu help = new Menu();
+	@FXML
+	Menu search = new Menu();
+	@FXML
+	Menu update = new Menu();
+	@FXML
+	Menu category = new Menu();
+	@FXML
+	MenuBar menu = new MenuBar();
+	@FXML
+	MenuItem editItem = new MenuItem();
+	@FXML
+	MenuItem profileItem = new MenuItem();
+	@FXML
+	MenuItem deleteItem = new MenuItem();
+	@FXML
+	MenuItem updateItem = new MenuItem();
+	@FXML
+	MenuItem viewItem = new MenuItem();
+	@FXML
+	MenuItem viewIndividualItem = new MenuItem();
+	@FXML
+	MenuItem searchPublisher = new MenuItem();
+	@FXML
+	MenuItem searchAuthor = new MenuItem();
+	@FXML
+	MenuItem searchISBN_Title = new MenuItem();
+	@FXML
+	MenuItem insertItem = new MenuItem();
+	@FXML
+	MenuItem science = new MenuItem();
+	@FXML
+	MenuItem art = new MenuItem();
+	@FXML
+	MenuItem history = new MenuItem();
+	@FXML
+	MenuItem religion = new MenuItem();
+	@FXML
+	MenuItem geography = new MenuItem();
+	@FXML
+	TextField publisherText = new TextField();
+	@FXML
+	TextField authorText = new TextField();
+	@FXML
+	TextField ISBN_Title_Text = new TextField();
+	@FXML
+	Label shippingCart = new Label();
+	@FXML
+	ListView<String> list_Books = new ListView<String>();
 	
     public void editInformation(ActionEvent event) throws IOException {
-    	
+    	Parent root = FXMLLoader.load(getClass().getResource("Edit.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
 	}
 	
 	public void search_forBook(ActionEvent event) throws IOException {
@@ -58,6 +123,7 @@ public class ControllerCustomers {
 		Stage app = (Stage) ((Node) (event.getSource())).getScene().getWindow();
 		app.setScene(scene);
 		app.show();
+		
 	}
     
    public void Manage1(ActionEvent event) throws IOException {
--- Order_Processing/src/ControllerMangers.java
+++ Order_Processing/src/ControllerMangers.java
@@ -13,7 +13,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.layout.GridPane;
@@ -21,26 +25,101 @@ import javafx.stage.Stage;
 
 public class ControllerMangers {
 
-	@FXML
-	Button manager = new Button();
-	@FXML
-	Button edit = new Button();
-	@FXML
-	Button search = new Button();
-	@FXML
-	Button add = new Button();
-	@FXML
 	Button log_out = new Button();
 	@FXML
-	MenuButton manage = new MenuButton();
+	Menu edit = new Menu();
 	@FXML
-	MenuButton checkout = new MenuButton();
-	
-	public void options(ActionEvent event) throws IOException {
-		
-	}
+	Menu profile = new Menu();
+	@FXML
+	Menu insert = new Menu();
+	@FXML
+	Menu delete = new Menu();
+	@FXML
+	Menu view = new Menu();
+	@FXML
+	Menu help = new Menu();
+	@FXML
+	Menu search = new Menu();
+	@FXML
+	Menu update = new Menu();
+	@FXML
+	Menu category = new Menu();
+	@FXML
+	Menu add = new Menu();
+	@FXML
+	Menu modify = new Menu();
+	@FXML
+	Menu placeOrder = new Menu();
+	@FXML
+	Menu confirmOrder = new Menu();
+	@FXML
+	Menu viewReport = new Menu();
+	@FXML
+	MenuBar menu = new MenuBar();
+	@FXML
+	MenuItem editItem = new MenuItem();
+	@FXML
+	MenuItem profileItem = new MenuItem();
+	@FXML
+	MenuItem deleteItem = new MenuItem();
+	@FXML
+	MenuItem updateItem = new MenuItem();
+	@FXML
+	MenuItem viewItem = new MenuItem();
+	@FXML
+	MenuItem viewIndividualItem = new MenuItem();
+	@FXML
+	MenuItem searchPublisher = new MenuItem();
+	@FXML
+	MenuItem searchAuthor = new MenuItem();
+	@FXML
+	MenuItem searchISBN_Title = new MenuItem();
+	@FXML
+	MenuItem insertItem = new MenuItem();
+	@FXML
+	MenuItem science = new MenuItem();
+	@FXML
+	MenuItem art = new MenuItem();
+	@FXML
+	MenuItem history = new MenuItem();
+	@FXML
+	MenuItem religion = new MenuItem();
+	@FXML
+	MenuItem geography = new MenuItem();
+	@FXML
+	MenuItem addItem = new MenuItem();
+	@FXML
+	MenuItem modifyItem = new MenuItem();
+	@FXML
+	MenuItem placeOrderItem = new MenuItem();
+	@FXML
+	MenuItem confirmOrderItem = new MenuItem();
+	@FXML
+	MenuItem totalSalesReport = new MenuItem();
+	@FXML
+	MenuItem topFiveCustomerReport = new MenuItem();
+	@FXML
+	MenuItem topTenBooksReport = new MenuItem();
+	@FXML
+	TextField publisherText = new TextField();
+	@FXML
+	TextField authorText = new TextField();
+	@FXML
+	TextField ISBN_Title_Text = new TextField();
+	@FXML
+	Label shippingCart = new Label();
+	@FXML
+	ListView<String> list_Books = new ListView<String>();
 	
     public void editInformation(ActionEvent event) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("Edit.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+	}
+	
+	public void options(ActionEvent event) throws IOException {
 		
 	}
 	
