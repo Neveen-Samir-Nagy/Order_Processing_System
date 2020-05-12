434fd1c8c26ed5464fabbfcdbeb2de7fd321248d
Febronia <febroniaashraf27@gmail.com>
2020-05-13 01:14:01 +0200

    add_book

--- Order_Processing/src/Controller_AddNewBook.java
+++ Order_Processing/src/Controller_AddNewBook.java
@@ -1,8 +1,17 @@
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -40,11 +49,36 @@ public class Controller_AddNewBook {
 	TextField categoryText = new TextField();
 	@FXML
 	TextField authorText = new TextField();
-	
+	@FXML
+	ObservableList<String> categoryOptions = 
+    FXCollections.observableArrayList(
+    		"Science", "Art", "Religion", "History" , "Geography"
+    );
+	final ComboBox comboBox = new ComboBox(categoryOptions);
 	SingletonClasses s2 = SingletonClasses.getoneclass();
+	Manager m = new Manager();
 	
 	public void done(ActionEvent event) throws IOException {
+	  if(chek_digits(ISBNText.getText()) && chek_digits(priceText.getText())) {
+		int isbn = Integer.parseInt(ISBNText.getText()), p=Integer.parseInt(priceText.getText());
+		String t = titleText.getText(), auth =  authorText.getText(), pub =  publisherText.getText(), y = yearText.getText(),
+				cat= categoryText.getText();
+		
+		Book b = new Book(isbn, t, auth, pub, y, p, cat);
+		m.add_NewBook(b);
 		Stage stage = (Stage) add.getScene().getWindow();
 	    stage.close();
+	  }
+	  else {
+		  Alert a = new Alert(AlertType.ERROR);
+		  a.setContentText("ISBN and Price must be a number"); 
+          a.show(); 
+	  }
+	}
+	public boolean chek_digits(String s) {
+		String pattern = "\\w+";
+	      Pattern r = Pattern.compile(pattern);
+	      Matcher m = r.matcher(s);
+	      return !m.find();
 	}
 }
