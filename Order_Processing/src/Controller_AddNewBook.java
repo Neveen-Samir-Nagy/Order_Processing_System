6480396db3b4a281ebf5aaed99a3531506c3231b
Febronia <febroniaashraf27@gmail.com>
2020-05-14 15:29:15 +0200

    d

--- Order_Processing/src/Controller_AddNewBook.java
+++ Order_Processing/src/Controller_AddNewBook.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+<<<<<<< HEAD
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,6 +11,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+=======
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+>>>>>>> parent of c6559be... Update
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -48,6 +56,7 @@ public class Controller_AddNewBook {
 	TextField categoryText = new TextField();
 	@FXML
 	TextField authorText = new TextField();
+<<<<<<< HEAD
 
 	Manager m = new Manager();
 	
@@ -73,5 +82,49 @@ public class Controller_AddNewBook {
 	      Pattern r = Pattern.compile(pattern);
 	      Matcher m = r.matcher(s);
 	      return !m.find();
+=======
+	
+	SingletonClasses s2 = SingletonClasses.getoneclass();
+	
+	public void done(ActionEvent event) throws IOException {
+		if(s2.get_book().equals("add")) {
+			s2.manager.add_NewBook(ISBNText.getText(), titleText.getText(), publisherText.getText(),
+					authorText.getText(), yearText.getText(), priceText.getText());
+		}else if(s2.get_book().equals("modify")) {
+			Queue<String> column = new LinkedList<String>();
+			Queue<String> value = new LinkedList<String>();
+			if(ISBNText.getText() != "Null") {
+				column.add("ISBN");
+				value.add(ISBNText.getText());
+			}
+			if(titleText.getText() != "Null") {
+				column.add("Tilte");
+				value.add(titleText.getText());
+			}
+			if(categoryText.getText() != "Null") {
+				column.add("Category");
+				value.add(categoryText.getText());
+			}
+			if(publisherText.getText() != "Null") {
+				column.add("Publisher_Name");
+				value.add(publisherText.getText());
+			}
+			if(yearText.getText() != "Null") {
+				column.add("Publication_Year");
+				value.add(yearText.getText());
+			}
+			if(priceText.getText() != "Null") {
+				column.add("Selling_Price");
+				value.add(priceText.getText());
+			}
+			if(authorText.getText() != "Null") {
+				column.add("Author");
+				value.add(authorText.getText());
+			}
+			s2.manager.modify_ExistingBook(s2.get_column_book(), s2.get_value_book(), column, value);
+		}
+		Stage stage = (Stage) add.getScene().getWindow();
+	    stage.close();
+>>>>>>> parent of c6559be... Update
 	}
 }
---
+++ Order_Processing/src/Manager.java
@@ -0,0 +1,37 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Queue;
+
+public class Manager implements Managers{
+
+	@Override
+	public void add_NewBook(String ISBN, String Title, String Publisher, String Author, String year, String price) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void modify_ExistingBook(String column, String value, Queue update_columns, Queue update_values) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void place_OrdersForBooks() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void promote_RegisteredCustomersToHaveManagersCredentials() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void view_GroupingOnSales() {
+		// TODO Auto-generated method stub
+		
+	}
+}
